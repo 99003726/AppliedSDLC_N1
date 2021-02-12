02312a8dac339a349a6ec8351110ba12d1db7ece
99003726 <abhishek.guria@ltts.com>
2021-02-13 00:08:36 +0530

    AppliedSDLC_N1 bacth git repo has been created

---
+++ 3_Implementation/src/calculator_operations.c
@@ -0,0 +1,89 @@
+#include <calculator_operations.h>
+
+int add(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int subtract(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int multiply(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+logBase10 = pow(10,x);
+int GCD(int operand1, int operand2)
+{
+   int i, GCD;
+
+  
+    for(i = 1; i <= operand1 && i <= operand2; i++)
+    {
+        if(operand1 % i == 0 && operand2 % i == 0)
+            GCD = i;
+    }
+
+     
+    return  GCD;
+}
+
+float LOG(float operand1)
+{
+ 
+        return log(operand1);
+}
+
+float ANTILOG(float operand1)
+{
+
+}
+
+float SINE(float operand1)
+{
+ float ret, val;
+
+
+   val = 3.141592/ 180;
+   ret = sin(operand1*val);
+  
+   
+   return(ret);
+       
+}
+
+
+float COS(float operand1)
+{
+ float ret, val;
+
+
+   val = 3.141592/ 180;
+   ret = cos(operand1*val);
+  
+   
+   return(ret);
+       
+}
+
+
+float TAN(float operand1)
+{
+ float ret, val;
+
+
+   val = 3.141592/ 180;
+   ret = tan(operand1*val);
+  
+   
+   return(ret);
+       
+}
+
+float power(float operand1, float operand2)
+{
+    return(pow(float operand1, float operand2));
+}
